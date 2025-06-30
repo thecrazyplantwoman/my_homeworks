da712a9d1c665657b62d711665213e0766163282
Maryna Tropanets <m.halimanenko@gmail.com>
2025-06-30 11:51:31 +0300

    Function updated

--- hw_6.1.js
+++ hw_6.1.js
@@ -1,2 +1,25 @@
-let rnd = Math.random();
-console.log (rnd);
+function generateRandomNumbers(count) {
+  const numbers = [];
+  let evenCount = 0;
+  let oddCount = 0;
+
+  for (let i = 0; i < count; i++) {
+    const randomNum = Math.floor(Math.random() * (1000 - 100 + 1)) + 100;
+    numbers.push(randomNum);
+
+    if (randomNum % 2 === 0) {
+      evenCount++;
+    } else {
+      oddCount++;
+    }
+  }
+
+  const evenPercent = (evenCount / count) * 100;
+  const oddPercent = (oddCount / count) * 100;
+
+  console.log("Згенеровані числа: " + numbers);
+  console.log("Парні: " + evenCount + " (" + evenPercent.toFixed(0) + "%)");
+  console.log("Непарні: " + oddCount + " (" + oddPercent.toFixed(0) + "%)");
+}
+
+generateRandomNumbers(10);
